0a58a62d03c3a4c04e87d09d508af2ae7d95388c
Infinity <halvi.rahman@gmail.com>
2022-01-14 01:53:24 +0700

    menambah program aktivasi user pada admin

---
+++ admin/aktivasiuser-detail.php
@@ -0,0 +1,160 @@
+<?php
+session_start();
+$userid = $_SESSION['userid'];
+global $userid;
+$role = $_SESSION['role'];
+$jabatan = $_SESSION['jabatan'];
+$nama = $_SESSION['nama'];
+$nim = $_SESSION['nim'];
+
+if ($role != 'admin') {
+  header("location:../deauth.php");
+}
+require('../config.php');
+require('../vendor/myfunc.php');
+
+
+if (isset($_POST['edit'])) {
+
+  $aktif = mysqli_real_escape_string($conn, $_POST['aktif']);
+  $token = mysqli_real_escape_string($conn, $_POST['token']);
+
+  $stmt = $conn->prepare("UPDATE pengguna
+                              SET aktif=?
+                              WHERE token=?");
+  $stmt->bind_param("ss", $aktif, $token);
+  $stmt->execute();
+  header('location:aktivasiuser.php?pesan=success');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title>Manajemen Skripsi</title>
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="../css/ruang-admin.min.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <?php
+    require('sidebar.php');
+    ?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+        <?php
+        require('topbar.php');
+        ?>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Edit Pengguna Sistem</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Dashboard</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Edit Pengguna Sistem</li>
+            </ol>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <!-- Form Basic -->
+              <div class="card mb-12">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Data Profil</h6>
+                </div>
+
+                <?php
+                // ambil data pengajuan judul
+                $token = $_GET['token'];
+                $stmt = $conn->prepare("SELECT * FROM pengguna WHERE token=?");
+                $stmt->bind_param("s", $token);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $dhasil = $result->fetch_assoc();
+                $nama = $dhasil['nama'];
+                $nim = $dhasil['nim'];
+                $nohp = $dhasil['nohp'];
+                $email = $dhasil['email'];
+                $userid = $dhasil['userid'];
+                $aktif = $dhasil['aktif'];
+                $token = $dhasil['token'];
+                ?>
+
+                <div class="card-body">
+
+                  <form action="" enctype="multipart/form-data" method="POST">
+                    <input type="hidden" name="token" value="<?= $token; ?>">
+                    <div class="form-group">
+                      <label>Nama</label>
+                      <input type="text" class="form-control" value="<?= $nama; ?>" name="nama" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label>NIM / NIP / NIPT</label>
+                      <input type="number" class="form-control" value="<?= $nim; ?>" name="nim" readonly>
+                    </div>
+                    <div class="form-group row">
+                      <div class="col-sm-6 mb-3 mb-sm-0">
+                        <label>No. HP / Whatsapp</label>
+                        <input type="number" class="form-control" value="<?= $nohp; ?>" name="nohp" readonly>
+                      </div>
+                      <div class="col-sm-6">
+                        <label>Email</label>
+                        <input type="email" class="form-control" value="<?= $email; ?>" name="email" readonly>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label>User ID</label>
+                      <input type="text" class="form-control" value="<?= $userid; ?>" name="userid" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label>Status User</label>
+                      <select name="aktif" class="form-control">
+                        <option value="0" selected>Tidak Aktif</option>
+                        <option value="1">Aktif</option>
+                      </select>
+                    </div>
+
+                    <button type="submit" name="edit" class="btn btn-warning btn-lg btn-block">Aktifkan Akun</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--Row-->
+        </div>
+      </div>
+      <!-- Footer -->
+      <?php
+      require('footer.php');
+      ?>
+      <!-- Footer -->
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="../js/ruang-admin.min.js"></script>
+
+</body>
+
+</html>
--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,49 +1,60 @@
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-        <div class="sidebar-brand-icon">
-            <img src="../img/uinlogo-small.png">
-        </div>
-        <div class="sidebar-brand-text mx-3">Manajemen SKRIPSI</div>
-    </a>
-    <hr class="sidebar-divider my-0">
-    <li class="nav-item active">
-        <a class="nav-link" href="index.php">
-            <i class="fa fa-desktop"></i>
-            <span>Dashboard</span></a>
-    </li>
-    <hr class="sidebar-divider">
-    <div class="sidebar-heading">
-        Jadwal Ujian
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <div class="sidebar-brand-icon">
+      <img src="../img/uinlogo-small.png">
     </div>
-    <li class="nav-item">
-        <a class="nav-link" href="judulproposal-isi.php">
-            <i class="fas fa-fw fa-file"></i>
-            <span>Judul Proposal</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="ujianproposal-isi.php">
-            <i class="fas fa-fw fa-comments"></i>
-            <span>Ujian Proposal</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="ujiankompre-isi.php">
-            <i class="fas fa-fw fa-comments"></i>
-            <span>Ujian Komprehensif</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="seminarhasil-isi.php">
-            <i class="fas fa-fw fa-comments"></i>
-            <span>Seminar Hasil</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="sidangskripsi-isi.php">
-            <i class="fas fa-fw fa-gavel"></i>
-            <span>Sidang Skripsi</span>
-        </a>
-    </li>
-    <hr class="sidebar-divider">
+    <div class="sidebar-brand-text mx-3">Manajemen SKRIPSI</div>
+  </a>
+  <hr class="sidebar-divider my-0">
+  <li class="nav-item active">
+    <a class="nav-link" href="index.php">
+      <i class="fa fa-desktop"></i>
+      <span>Dashboard</span></a>
+  </li>
+  <hr class="sidebar-divider">
+  <div class="sidebar-heading">
+    Jadwal Ujian
+  </div>
+  <li class="nav-item">
+    <a class="nav-link" href="judulproposal-isi.php">
+      <i class="fas fa-fw fa-file"></i>
+      <span>Judul Proposal</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="ujianproposal-isi.php">
+      <i class="fas fa-fw fa-comments"></i>
+      <span>Ujian Proposal</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="ujiankompre-isi.php">
+      <i class="fas fa-fw fa-comments"></i>
+      <span>Ujian Komprehensif</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="seminarhasil-isi.php">
+      <i class="fas fa-fw fa-comments"></i>
+      <span>Seminar Hasil</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="sidangskripsi-isi.php">
+      <i class="fas fa-fw fa-gavel"></i>
+      <span>Sidang Skripsi</span>
+    </a>
+  </li>
+  <p></p>
+  <hr class="sidebar-divider my-0">
+  <!-- <div class="sidebar-heading">
+    User
+  </div> -->
+  <li class="nav-item">
+    <a class="nav-link" href="aktivasiuser.php">
+      <i class="fas fa-fw fa-user"></i>
+      <span>Aktivasi User</span>
+    </a>
+  </li>
+  <hr class="sidebar-divider">
 </ul>
